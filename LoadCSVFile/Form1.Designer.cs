b0795228eac29f0114a6932afc11d88a072c5781
Sayyab Khan <sayyabkhan007@gmail.com>
2023-11-13 21:22:33 +0500

    Add project files.

---
+++ LoadCSVFile/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace LoadCSVFile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtBrowseFile = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.lblLoadData = new System.Windows.Forms.Label();
+            this.btnLoadData = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 288);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(776, 250);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // txtBrowseFile
+            // 
+            this.txtBrowseFile.Location = new System.Drawing.Point(162, 51);
+            this.txtBrowseFile.Name = "txtBrowseFile";
+            this.txtBrowseFile.Size = new System.Drawing.Size(404, 20);
+            this.txtBrowseFile.TabIndex = 1;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBrowse.Location = new System.Drawing.Point(281, 92);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(164, 32);
+            this.btnBrowse.TabIndex = 2;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.Location = new System.Drawing.Point(695, 237);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(93, 30);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(12, 247);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(677, 20);
+            this.txtSearch.TabIndex = 0;
+            // 
+            // lblLoadData
+            // 
+            this.lblLoadData.AutoSize = true;
+            this.lblLoadData.Font = new System.Drawing.Font("Stencil", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLoadData.Location = new System.Drawing.Point(254, 9);
+            this.lblLoadData.Name = "lblLoadData";
+            this.lblLoadData.Size = new System.Drawing.Size(228, 25);
+            this.lblLoadData.TabIndex = 3;
+            this.lblLoadData.Text = "Load CSV File Data";
+            // 
+            // btnLoadData
+            // 
+            this.btnLoadData.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLoadData.Location = new System.Drawing.Point(281, 130);
+            this.btnLoadData.Name = "btnLoadData";
+            this.btnLoadData.Size = new System.Drawing.Size(164, 32);
+            this.btnLoadData.TabIndex = 4;
+            this.btnLoadData.Text = "Load Data";
+            this.btnLoadData.UseVisualStyleBackColor = true;
+            this.btnLoadData.Click += new System.EventHandler(this.btnLoadData_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 550);
+            this.Controls.Add(this.btnLoadData);
+            this.Controls.Add(this.lblLoadData);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.txtBrowseFile);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtBrowseFile;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Label lblLoadData;
+        private System.Windows.Forms.Button btnLoadData;
+    }
+}
+
---
+++ LoadCSVFile/Form1.cs
@@ -0,0 +1,140 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoadCSVFile
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void LoadData(string path)
+        {
+            if(!string.IsNullOrEmpty(path))
+            {
+                //using (StreamReader readFile = new StreamReader(path))
+                using (TextFieldParser parseFiled = new TextFieldParser(path))
+                {
+                    //shows that the data is delimited through commas
+                    parseFiled.TextFieldType = FieldType.Delimited;
+                    parseFiled.SetDelimiters(",");
+
+
+                    //print column names
+                    String[] headers = parseFiled.ReadFields();
+
+                    foreach (String header in headers)
+                    {
+                        dataGridView1.Columns.Add(header, header);
+
+                    }
+
+
+                    //print rows
+                    while (!parseFiled.EndOfData)
+                    {
+                        string[] rows = parseFiled.ReadFields();
+
+                        dataGridView1.Rows.Add(rows);
+                    }
+
+
+                }
+
+
+            }
+
+            else
+            {
+                MessageBox.Show("Please upload a file first.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+
+        //upload a file to display its data
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.ShowDialog();
+            txtBrowseFile.Text = openFileDialog.FileName;
+        }
+
+
+        //Display the uploaded file data in a gridView
+        private void btnLoadData_Click(object sender, EventArgs e)
+        {
+           LoadData(txtBrowseFile.Text);
+            txtBrowseFile.Clear();
+        }
+
+
+        //Search a record
+        private void Search(string record)
+        {
+            dataGridView1.ClearSelection();
+
+            // Variable to track if any matching record is found
+            bool matchFound = false;
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                if (!row.IsNewRow)
+                {
+                    for (int i = 0; i < row.Cells.Count; i++)
+                    {
+                        if (row.Cells[i].Value != null && row.Cells[i].Value.ToString().Equals(record, StringComparison.OrdinalIgnoreCase))
+                        {
+                            row.Visible = true;
+                            matchFound = true; 
+                            break;
+                        }
+                        else
+                        {
+                            // Hide non-matching rows
+                            row.Visible = false; 
+                        }
+                    }
+                }
+            }
+
+                
+                if (!matchFound)
+                {
+                    MessageBox.Show("Record not found!!!", "Record Search", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+
+        }
+
+
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string searchRecord = txtSearch.Text.Trim();
+
+            if (!string.IsNullOrEmpty(searchRecord))
+            {
+                Search(searchRecord);
+            }
+            else
+            {
+                MessageBox.Show("Please enter into the Search bar.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+       
+    }
+}
